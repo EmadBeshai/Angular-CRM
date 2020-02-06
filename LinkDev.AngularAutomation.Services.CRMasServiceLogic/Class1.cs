4e0b72f6fce55793814c426251300e19961b076f
Emad Beshai <Emad.Beshai@outlook.com>
2020-02-06 20:42:15 +0200

    Genrate CRM Contracts

---
+++ LinkDev.AngularAutomation.Services.CRMasServiceLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.AngularAutomation.Services.CRMasServiceLogic
+{
+    public class Class1
+    {
+    }
+}
